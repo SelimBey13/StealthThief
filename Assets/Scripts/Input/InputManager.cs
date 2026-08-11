9907d6319a8502c9c8539c198405c98711b5d37a
Selim Kadir Yasar <yasary5775@gmail.com>
2026-08-11 10:53:58 +0300

    Input Manager sistemi eklendi

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,171 @@
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance {get; private set;}
+    private InputActions inputActions;
+    public static event Action<Vector2> OnMove;
+    public static event Action<bool> OnSprint;
+    public static event Action<bool> OnCrouch;
+    public static event Action<bool> OnInteract;
+    public static event Action<bool> OnFire;
+    public static event Action<bool> OnAim;
+    public static event Action<Vector2> OnLook;
+
+    void Awake()
+    {
+        AwakingController();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable(); // bu sistem daha sonra değiştirilecek,menü mantıgı
+
+        inputActions.Player.Move.performed += MovePerformed;
+        inputActions.Player.Move.canceled += MoveCanceled;
+
+        inputActions.Player.Sprint.performed += SprintPerformed;
+        inputActions.Player.Sprint.canceled += SprintCanceled;
+        
+        inputActions.Player.Crouch.performed += CrouchPerformed;
+        inputActions.Player.Crouch.canceled += CrouchCanceled;
+
+        inputActions.Player.Interact.performed += InteractPerformed;
+        inputActions.Player.Interact.canceled += InteractCanceled;
+
+        inputActions.Player.Fire.performed += FirePerformed;
+        inputActions.Player.Fire.canceled += FireCanceled;
+
+        inputActions.Player.Aim.performed += AimPerformed;
+        inputActions.Player.Aim.canceled += AimCanceled;
+        
+        inputActions.Player.Look.performed += LookPerformed;
+        inputActions.Player.Look.canceled += LookCanceled;
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable(); // bu sistem daha sonra değiştirilecek,menü mantıgı
+
+        inputActions.Player.Move.performed -= MovePerformed;
+        inputActions.Player.Move.canceled -= MoveCanceled;
+
+        inputActions.Player.Sprint.performed -= SprintPerformed;
+        inputActions.Player.Sprint.canceled -= SprintCanceled;
+
+        inputActions.Player.Crouch.performed -= CrouchPerformed;
+        inputActions.Player.Crouch.canceled -= CrouchCanceled;
+        
+        inputActions.Player.Interact.performed -= InteractPerformed;
+        inputActions.Player.Interact.canceled -= InteractCanceled;
+
+        inputActions.Player.Fire.performed -= FirePerformed;
+        inputActions.Player.Fire.canceled -= FireCanceled;
+        
+        inputActions.Player.Aim.performed -= AimPerformed;
+        inputActions.Player.Aim.canceled -= AimCanceled;
+
+        inputActions.Player.Look.performed -= LookPerformed;
+        inputActions.Player.Look.canceled -= LookCanceled;
+    }
+
+    void AwakingController()
+    {
+        if(Instance!=null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        inputActions = new InputActions();
+    }
+
+    #region Move
+    void MovePerformed(InputAction.CallbackContext ctx)
+    {
+        OnMove?.Invoke(ctx.ReadValue<Vector2>());
+    }
+    void MoveCanceled(InputAction.CallbackContext ctx)
+    {
+        OnMove?.Invoke(Vector2.zero);
+    }
+
+    #endregion
+
+    #region Sprint
+    void SprintPerformed(InputAction.CallbackContext ctx)
+    {
+        OnSprint?.Invoke(true);
+    }
+    void SprintCanceled(InputAction.CallbackContext ctx)
+    {
+        OnSprint?.Invoke(false);
+    }
+    #endregion
+
+    #region Crouch
+    void CrouchPerformed(InputAction.CallbackContext ctx)
+    {
+        OnCrouch?.Invoke(true);
+    }
+    void CrouchCanceled(InputAction.CallbackContext ctx)
+    {
+        OnCrouch?.Invoke(false);
+    }
+    #endregion
+
+    #region Interact
+
+    void InteractPerformed(InputAction.CallbackContext ctx)
+    {
+        OnInteract?.Invoke(true);
+    }
+    void InteractCanceled(InputAction.CallbackContext ctx)
+    {
+        OnInteract?.Invoke(false);
+    }
+
+    #endregion
+
+    #region Fire
+
+    void FirePerformed(InputAction.CallbackContext ctx)
+    {
+        OnFire?.Invoke(true);
+    }
+    void FireCanceled(InputAction.CallbackContext ctx)
+    {
+        OnFire?.Invoke(false);
+    }
+
+    #endregion
+
+    #region Aim
+
+    void AimPerformed(InputAction.CallbackContext ctx)
+    {
+        OnAim?.Invoke(true);
+    }
+    void AimCanceled(InputAction.CallbackContext ctx)
+    {
+        OnAim?.Invoke(false);
+    }
+
+    #endregion
+
+    #region Look
+
+    void LookPerformed(InputAction.CallbackContext ctx)
+    {
+        OnLook?.Invoke(ctx.ReadValue<Vector2>());
+    }
+    void LookCanceled(InputAction.CallbackContext ctx)
+    {
+        OnLook?.Invoke(Vector2.zero);
+    }
+
+    #endregion
+}
